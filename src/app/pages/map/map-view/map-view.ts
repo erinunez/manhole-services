fa626a32bba49336a98ee20942de3c5ad49cad8a
erinunez <zaireenakhzar@gmail.com>
2025-09-17 23:55:26 +0800

    add navbar

--- src/app/pages/map/map-view/map-view.ts
+++ src/app/pages/map/map-view/map-view.ts
@@ -1,23 +1,30 @@
 import { Component, AfterViewInit } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import * as L from 'leaflet';
+import { Navbar } from '../navbar/navbar';
 
 @Component({
-  selector: 'app-map-view',
-  imports: [ButtonModule],
-  templateUrl: './map-view.html',
-  styleUrl: './map-view.scss'
+    selector: 'app-map-view',
+    imports: [ButtonModule, Navbar],
+    templateUrl: './map-view.html',
+    styleUrl: './map-view.scss'
 })
 export class MapView implements AfterViewInit {
-ngAfterViewInit(): void {
-    const map = L.map('map').setView([2.9264, 101.6964], 16);
+    ngAfterViewInit(): void {
+       const map = L.map('map', {
+  zoomControl: false // 🚫 disables default zoom control
+}).setView([2.9264, 101.6964], 16);
 
 L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
   maxZoom: 18,
   attribution: '© OpenStreetMap contributors © CARTO',
   subdomains: 'abcd',
-  noWrap: true    // 🔥 stops repeating horizontally
+  noWrap: true
 }).addTo(map);
-  }
-  
+
+L.control.zoom({
+  position: 'bottomright' // ✅ now only one control
+}).addTo(map);
+
+    }
 }
